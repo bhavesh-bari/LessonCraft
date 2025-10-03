c6358d1f0aecead113b38c9005de8ff02b12319b
bhavesh-bari <bhaveshbari0402@gmail.com>
2025-10-03 10:36:17 +0530

    build error

--- src/app/api/google/status/route.js
+++ src/app/api/google/status/route.js
@@ -1,14 +1,14 @@
-// src/app/api/google/status/route.js
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import dbConnect from "@/lib/db";
-import User from "@/models/User";
+// // src/app/api/google/status/route.js
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+// import dbConnect from "@/lib/db";
+// import User from "@/models/User";
 
-export async function GET() {
-    const session = await getServerSession(authOptions);
-    if (!session) return Response.json({ connected: false });
+// export async function GET() {
+//     const session = await getServerSession(authOptions);
+//     if (!session) return Response.json({ connected: false });
 
-    await dbConnect();
-    const user = await User.findOne({ email: session.user.email });
-    return Response.json({ connected: !!user?.googleAccessToken });
-}
+//     await dbConnect();
+//     const user = await User.findOne({ email: session.user.email });
+//     return Response.json({ connected: !!user?.googleAccessToken });
+// }
